dbc9cebc2d5c814e38787a53137c42b322280202
FranziskaSW <franziska.wehrmann@gmail.com>
2019-01-05 01:26:13 +0200

    Zipfian plot - rank of words

--- text_mining.py
+++ text_mining.py
@@ -4,6 +4,7 @@ import nltk
 # nltk.download('punkt')
 # nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import re
 import pandas as pd
 from collections import Counter
@@ -35,6 +36,14 @@ if __name__ == '__main__':
         plain_text = f.read().replace('\n', ' ')
         plain_words_lst = re.sub("[^\w]", " ", plain_text).split()
 
+        space = '                                ' # 32 spaces
+        clean_text = plain_text.translate(str.maketrans(string.punctuation, space)).lower()
+
+        tokenized_text = nltk.word_tokenize(clean_text)
+        tokens = pd.Series(tokenized_text).value_counts()
+
+
+
         text_pos = nltk.pos_tag(plain_words_lst)
         noun_phrases = extract_noun_phrase(text_pos)
 
@@ -45,12 +54,6 @@ if __name__ == '__main__':
         plt.show()
 
         # start of (b)
-
-        space = '                                ' # 32 spaces
-        clean_text = plain_text.translate(str.maketrans(string.punctuation, space)).lower()
-
-        tokenized_text = nltk.word_tokenize(clean_text)
-        tokens = pd.Series(tokenized_text).value_counts()
 
         # lowercase_text = [w.lower() for w in tokenized_text]
         # tokens_lc = pd.Series(lowercase_text).value_counts()
@@ -75,7 +78,6 @@ if __name__ == '__main__':
 
         # (d) stemming
 
-        from nltk.stem import PorterStemmer
         ps = PorterStemmer()
 
         stemmed_text = [ps.stem(w) for w in filtered_text]
@@ -121,5 +123,30 @@ if __name__ == '__main__':
 
 
         # (d) sentences
+
+
+
+    # A Zipf plot
+    counts = tokens_filtered
+    indices = tokens_filtered.index
+
+    ranks = np.arange(1, len(counts)+1)
+    #indices = np.argsort(-counts)
+    #frequencies = counts[indices]
+
+    frequencies = counts
+
+    plt.loglog(ranks, frequencies, marker=".")
+    plt.title("Zipf plot for Nietzsche tokens")
+    plt.xlabel("Frequency rank of token")
+    plt.ylabel("Absolute frequency of token")
+    plt.grid(True)
+
+    for n in list(np.logspace(-0.5, np.log10(len(counts)), 20).astype(int)):
+        print(n)
+
+        dummy = plt.text(ranks[n], frequencies[n], " " + indices[n],
+                     verticalalignment="bottom",
+                     horizontalalignment="left")
 
 
