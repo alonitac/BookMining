a94199951d71056a75123c7ccbebc2b42b547688
FranziskaSW <franziska.wehrmann@gmail.com>
2019-01-05 19:25:52 +0200

    plot functions

--- text_mining.py
+++ text_mining.py
@@ -31,6 +31,79 @@ def extract_noun_phrase(pos_list):
             i += 1
     return phrases
 
+def bar_plot(tokens, tokens_filtered, tokens_stemmed):
+
+    fig1, (ax0, ax1, ax2) = plt.subplots(ncols=3, nrows=1, figsize=(16, 12))
+
+    ax0.set_title('unfiltered')
+    people = tokens[:50].index
+    y_pos = np.linspace(len(people),0)
+    ax0.barh(y_pos, tokens[:50])
+    ax0.set_xscale('log')
+    ax0.set_xlabel('occurrence')
+    ax0.set_yticks(y_pos)
+    ax0.set_yticklabels(people)
+
+    ax1.set_title('filtered')
+    people = tokens_filtered[:50].index
+    y_pos = np.linspace(len(people),0)
+    ax1.barh(y_pos, tokens_filtered[:50])
+    ax1.set_xscale('log')
+    ax1.set_xlabel('occurrence')
+    ax1.set_yticks(y_pos)
+    ax1.set_yticklabels(people)
+
+    ax2.set_title('filtered & stemmed')
+    people = tokens_stemmed[:50].index
+    y_pos = np.linspace(len(people),0)
+    ax2.barh(y_pos, tokens_stemmed[:50])
+    ax2.set_xscale('log')
+    ax2.set_xlabel('occurrence')
+    ax2.set_yticks(y_pos)
+    ax2.set_yticklabels(people)
+
+    plt.subplots_adjust(wspace=0.3)
+
+    return fig1
+
+
+def zipfian_plot(tokens, tokens_filtered, tokens_stemmed, tokens_np):
+
+    toks = [tokens, tokens_filtered, tokens_stemmed, tokens_np]
+
+    fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
+    fig1.subplots_adjust(hspace=0.3, wspace=0.05)
+
+    for ax, k in zip([ax1, ax2, ax3, ax4], toks):
+
+        print(ax)
+
+        tokens_list = k
+        counts = tokens_list
+        indices = tokens_list.index
+        ranks = np.arange(1, len(counts) + 1)
+        frequencies = counts
+
+
+        ax.loglog(ranks, frequencies, marker=".")
+
+        for n in list(np.logspace(-0.5, np.log10(len(counts)), 20, endpoint=False).astype(int)):
+            ax.text(ranks[n], frequencies[n], " " + indices[n],
+                         verticalalignment="bottom",
+                         horizontalalignment="left")
+
+    ax1.set_title("unfiltered")
+    ax2.set_title("filtered")
+    ax3.set_title("filtered & stemmed")
+    ax4.set_title("filtered & noun phrase")
+
+    ax3.set_xlabel("Frequency rank of token")
+    ax4.set_xlabel("Frequency rank of token")
+    ax1.set_ylabel("Absolute frequency of token")
+    ax3.set_ylabel("Absolute frequency of token")
+
+    return fig1
+
 
 if __name__ == '__main__':
 
@@ -88,158 +161,71 @@ if __name__ == '__main__':
 
     # nice to see that now philosoph = {philosophy, philosophers} and also moral = {moral, morality}
 
+    fig1 = bar_plot(tokens, tokens_filtered, tokens_stemmed)
 
-    fig1, (ax0, ax1, ax2) = plt.subplots(ncols=3, nrows=1, figsize=(16, 12))
 
-    ax0.set_title('unfiltered')
-    people = tokens[:50].index
-    y_pos = np.linspace(len(people),0)
-    ax0.barh(y_pos, tokens[:50])
-    ax0.set_xscale('log')
-    ax0.set_xlabel('occurrence')
-    ax0.set_yticks(y_pos)
-    ax0.set_yticklabels(people)
+    # (d) noun phrase
 
-    ax1.set_title('filtered')
-    people = tokens_filtered[:50].index
-    y_pos = np.linspace(len(people),0)
-    ax1.barh(y_pos, tokens_filtered[:50])
-    ax1.set_xscale('log')
-    ax1.set_xlabel('occurrence')
-    ax1.set_yticks(y_pos)
-    ax1.set_yticklabels(people)
+    pt = plain_text[545:]
 
-    ax2.set_title('filtered & stemmed')
-    people = tokens_stemmed[:50].index
-    y_pos = np.linspace(len(people),0)
-    ax2.barh(y_pos, tokens_stemmed[:50])
-    ax2.set_xscale('log')
-    ax2.set_xlabel('occurrence')
-    ax2.set_yticks(y_pos)
-    ax2.set_yticklabels(people)
+    space = '--'  # 32 spaces
+    pt = pt.translate(str.maketrans('[]', space))
+    pt = pt.lower() # try if tags different when lowercase first "SUPPOSING" YES! SUPPOSING - NN, supposing - vbg
+    sentences = sent_tokenize(pt)
 
-    plt.subplots_adjust(wspace=0.3)
+    # pos for sentences
+    sent_pos = [nltk.pos_tag(nltk.word_tokenize(sent)) for sent in sentences]
+
+    # find noun phrases
+    grammar = r"""
+      NP: {<JJ.*>+?<NN.*>+}
+          {<NN.*>+}
+    """
+
+    cp = nltk.RegexpParser(grammar)
+
+    np_text = []
+    for i in range(0, len(sent_pos)):
+        sentence = sent_pos[i]
+        noun_phrase = cp.parse(sent_pos[i])
+
+        branch = []
+        for subtree in noun_phrase.subtrees(filter=lambda t: t.label() == 'NP'):
+            k = subtree.leaves()
+            print(k)
+            phrase = [k[i][0] for i in range(0, len(k))]
+            separator = ', '
+            phrase = separator.join(phrase)
+            phrase = re.sub(', ', ' ', phrase)
+            print(phrase)
+            branch.append(phrase)
+
+        # combine to or-criterion for re.sub replacement
+        branch = separator.join(branch)
+        criterion = re.sub(', ', '|', branch)
+
+        # replace noun phrases by 'NP' token
+        new_sentence = re.sub(criterion, 'NP', sentences[i])  # would not work if there is something like back NN,  back VBZ
+        np_text.append(new_sentence)
+
+
+    # first combine all sentences back to text and remove punctuation
+    phrase = separator.join(np_text)
+    text = re.sub(', ', ' ', phrase)
+    space = '                                '  # 32 spaces
+    clean_text = text.translate(str.maketrans(string.punctuation, space))
+
+    tokenized_text = nltk.word_tokenize(clean_text)
+
+    filtered_text = [w for w in tokenized_text if not w in stop_words]
+    tokens_np = pd.Series(filtered_text).value_counts()
+
+    stemmed_text = [ps.stem(w) for w in filtered_text]
+    tokens_np = pd.Series(stemmed_text).value_counts()
+
+    fig2 =  zipfian_plot(tokens, tokens_filtered, tokens_stemmed, tokens_np)
 
     plt.show()
-
-
-
-
-    # (d) sentences
-
-
-
-    # A Zipf plot
-    tokens_list = tokens_stemmed
-    counts = tokens_list
-    indices = tokens_list.index
-
-    ranks = np.arange(1, len(counts)+1)
-    #indices = np.argsort(-counts)
-    #frequencies = counts[indices]
-
-    frequencies = counts
-    ax2.set_title('filtered & stemmed')
-    people = tokens_list[:50].index
-    y_pos = np.linspace(len(people),1, len(people))
-    ax2.barh(y_pos, tokens_list[:50])
-    ax2.set_xscale('log')
-    ax2.set_xlabel('occurrence')
-    ax2.set_yticks(y_pos)
-    plt.loglog(ranks, frequencies, marker=".")
-    plt.title("Zipf plot for Nietzsche tokens")
-    plt.xlabel("Frequency rank of token")
-    plt.ylabel("Absolute frequency of token")
-    plt.grid(True)
-
-    for n in list(np.logspace(-0.5, np.log10(len(counts)), 20).astype(int)):
-        print(n)
-
-        dummy = plt.text(ranks[n], frequencies[n], " " + indices[n],
-                     verticalalignment="bottom",
-                     horizontalalignment="left")
-
-
-# (d) noun phrase
-
-pt = plain_text[545:]
-
-space = '--'  # 32 spaces
-pt = pt.translate(str.maketrans('[]', space))
-pt = pt.lower() # try if tags different when lowercase first "SUPPOSING" YES! SUPPOSING - NN, supposing - vbg
-sentences = sent_tokenize(pt)
-
-period_sent = [re.split('\. ', sent) for sent in sentences]
-check_period = [group for group in period_sent if len(group)>1]
-
-# does good job
-check_period[1] # '...' but sometimes it should also end like this...
-check_period[3] # 'St. Augustine'
-check_period[5] # '92. Who has not' list
-check_period[6] # 'Nov' abriviation
-check_period[8] # 'Arther B. Bullock' names
-
-# pos for sentences
-sent_pos = [nltk.pos_tag(nltk.word_tokenize(sent)) for sent in sentences]
-
-extract_noun_phrase(sent_pos[3])
-
-
-len(period_sent)
-len(sent_tokenize_list)
-
-
-
-# test extract_noun_phrase function
-
-grammar = r"""
-  NP: {<JJ.*>+?<NN.*>+}
-      {<NN.*>+}
-"""
-
-cp = nltk.RegexpParser(grammar)
-
-np_text = []
-for i in range(0, len(sent_pos)):
-    sentence = sent_pos[i]
-    noun_phrase = cp.parse(sent_pos[i])
-
-    branch = []
-    for subtree in noun_phrase.subtrees(filter=lambda t: t.label() == 'NP'):
-        k = subtree.leaves()
-        print(k)
-        phrase = [k[i][0] for i in range(0, len(k))]
-        separator = ', '
-        phrase = separator.join(phrase)
-        phrase = re.sub(', ', ' ', phrase)
-        print(phrase)
-        branch.append(phrase)
-
-    # combine to or-criterion for re.sub replacement
-    branch = separator.join(branch)
-    criterion = re.sub(', ', '|', branch)
-
-    # replace noun phrases by 'NP' token
-    new_sentence = re.sub(criterion, 'NP', sentences[i])  # would not work if there is something like back NN,  back VBZ
-    np_text.append(new_sentence)
-
-
-# first combine all sentences back to text and remove punctuation
-phrase = separator.join(np_text)
-text = re.sub(', ', ' ', phrase)
-
-
-space = '                                '  # 32 spaces
-clean_text = text.translate(str.maketrans(string.punctuation, space))
-
-tokenized_text = nltk.word_tokenize(clean_text)
-
-filtered_text = [w for w in tokenized_text if not w in stop_words]
-tokens_filtered = pd.Series(filtered_text).value_counts()
-
-stemmed_text = [ps.stem(w) for w in filtered_text]
-tokens_stemmed = pd.Series(stemmed_text).value_counts()
-
 
 
 
